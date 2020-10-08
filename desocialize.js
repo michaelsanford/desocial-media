adb5889fb214045831cd9b66d8ac0518ecb58fb0
thepoppycat <47619363+thepoppycat@users.noreply.github.com>
2020-10-08 09:56:37 +0800

    Changed "a.remove()" to "display: none".

--- desocialize.js
+++ desocialize.js
@@ -4,9 +4,9 @@ const instagram = window.location.hostname == "www.instagram.com";
 const facebook = window.location.hostname == "www.facebook.com";
 
 window.addEventListener('scroll', () => {
-        instagram && [...document.getElementsByClassName('EtaWk')].forEach(a => a.remove());
-        facebook && [...document.getElementsByClassName('cwj9ozl2 tvmbv18p')].forEach(a => a.remove());
+        instagram && [...document.getElementsByClassName('EtaWk')].forEach(a => a.style.display = "none");
+        facebook && [...document.getElementsByClassName('cwj9ozl2 tvmbv18p')].forEach(a => a.style.display = "none");
 });
 
-instagram && [...document.getElementsByClassName('EtaWk')].forEach(a => a.remove());
-facebook && [...document.getElementsByClassName('cwj9ozl2 tvmbv18p')].forEach(a => a.remove());
+instagram && [...document.getElementsByClassName('EtaWk')].forEach(a => a.style.display = "none");
+facebook && [...document.getElementsByClassName('cwj9ozl2 tvmbv18p')].forEach(a => a.style.display = "none");
